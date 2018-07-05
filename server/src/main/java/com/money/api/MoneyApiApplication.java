94560c554c5df336bd6b87ff94dbae8fd56f5997
Jonathan931 <30840809+Jonathan931@users.noreply.github.com>
2018-07-05 17:18:43 -0300

    Escopo Inicial do Projeto Spring
    Carregando o escopo inicial do Projeto Spring parte do server

---
+++ server/src/main/java/com/money/api/MoneyApiApplication.java
@@ -0,0 +1,12 @@
+package com.money.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyApiApplication.class, args);
+	}
+}
