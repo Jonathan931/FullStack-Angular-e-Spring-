978c6e5c999f40c9f7a13d8ef0c60eaf1a5a7fd2
Jonathan <jonathanmatheus1997.jm@gmail.com>
2018-10-12 10:55:35 -0300

    Adicionando Refresh no Token

--- server/src/main/java/com/example/money/api/config/AuthorizationServerConfig.java
+++ server/src/main/java/com/example/money/api/config/AuthorizationServerConfig.java
@@ -25,8 +25,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.withClient("angular")
 			.secret("@ngular0")
 			.scopes("read", "write")
-			.authorizedGrantTypes("password")
-			.accessTokenValiditySeconds(1800);
+			.authorizedGrantTypes("password", "refresh_token")
+			.accessTokenValiditySeconds(1800)
+			.refreshTokenValiditySeconds(3600*24);
 	}
 	
 	@Override
@@ -34,6 +35,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints
 			.tokenStore(tokenStore())
 			.accessTokenConverter(accessTokenConverter())
+			.reuseRefreshTokens(false)
 			.authenticationManager(authenticationManager);
 	}
 	
